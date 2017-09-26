fc48560096e449b21a51c8e013af7c22940b11f3
Thiago Victorino <thiagovictorino@Thiagos-MacBook-Pro.local>
2017-09-26 17:03:04 -0300

    Fixing route

--- src/IAMServiceProvider.php
+++ src/IAMServiceProvider.php
@@ -1,15 +1,16 @@
 <?php
 namespace thiagovictorino\IAM;
 
-use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Support\ServiceProvider;
 
 use thiagovictorino\IAM\Services\IAMService;
 
-class IAMServiceProvider extends ServiceProvider {
+class IAMServiceProvider extends ServiceProvider  {
 
     protected $defer = true;
 
     public function boot() {
+
 
         $this->publishes([__DIR__.'/../config/iam.php' => config_path('iam.php')], 'iam');
 
@@ -30,6 +31,6 @@ class IAMServiceProvider extends ServiceProvider {
         });
     }
     public function provides() {
-        return ['iam'];
+        return ['thiagovictorino-iam'];
     }
 }
