9fe77421a9e5834d7393aefd05d64e3e4d627197
Thiago Victorino <thiagovictorino@Thiagos-MacBook-Pro.local>
2018-01-02 11:05:04 -0200

    POST login

--- routes/iam.php
+++ routes/iam.php
@@ -9,6 +9,7 @@ use thiagovictorino\IAM\Http\Middleware\IAMAuthMiddleware;
 
 Route::prefix('iam/v1/')->group(function () {
     Route::get('auth', IAMAuthController::class.'@auth');
+    Route::post('auth', IAMAuthController::class.'@auth');
 
     Route::group(['middleware' => [IAMAuthMiddleware::class.":IAM_read"]], function () {
         Route::prefix('user')->group(function(){
