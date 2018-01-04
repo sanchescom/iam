f48348aa4d6134a8d8fb21f72d6d962df25f98e2
Thiago Victorino <thiagovictorino@Thiagos-MacBook-Pro.local>
2018-01-04 13:18:44 -0200

    added CORS is middlware

--- src/Http/Middleware/IAMAuthMiddleware.php
+++ src/Http/Middleware/IAMAuthMiddleware.php
@@ -55,7 +55,12 @@ class IAMAuthMiddleware
             return response()->json($responseDTO,$responseDTO->code);
         }
 
-        return $next($request);
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    }
+}
+;
     }
 
     private function decodeToken($token): ?JwtDTO{
